04b7da85e71a441aafb91e85c92dc9501fe85c40
Roberto <roberto.brambilla@windtre.it>
2021-03-04 10:42:42 +0100

    Modfica Roberto Lunedi 1

---
+++ company-social-network/htdocs/typo3conf/ext/t3adminer/ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "t3adminer".
+ *
+ * Auto generated 04-03-2021 08:16
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'Adminer',
+  'description' => 'Database administration tool \'Adminer\'',
+  'category' => 'module',
+  'author' => 'Jigal van Hemert',
+  'author_email' => 'jigal.van.hemert@typo3.org',
+  'state' => 'stable',
+  'uploadfolder' => false,
+  'createDirs' => '',
+  'clearCacheOnLoad' => 0,
+  'author_company' => '',
+  'version' => '7.0.8',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '7.6.0-8.1.99',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'clearcacheonload' => false,
+);
+
