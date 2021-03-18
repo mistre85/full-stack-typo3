251ac1cd79e3195c4508e377454bfb431137af26
Paolo mistretta <mistre85@gmail.com>
2021-03-18 12:14:29 +0100

    esempio viewhelper lezione

--- lessons/mod2/OOP/ViewHelper.php
+++ lessons/mod2/OOP/ViewHelper.php
@@ -1,7 +1,54 @@
 <?php
 
 
-class ViewHelper
+abstract class AbstractViewHelper
 {
 
+    private $contenuto; // ??
+
+    public function setContent($contenuto)
+    {
+
+        //ragionamenti ??
+        $contenuto = $this->contenuto;
+    }
+
+    //abstract public function render();
 }
+
+class H1ViewHelper extends AbstractViewHelper
+{
+
+
+    public function render($page)
+    {
+
+        return "<h1>" . $this->titolo . "</h1>";
+    }
+}
+
+class PViewHelper extends AbstractViewHelper
+{
+
+
+    public function render()
+    {
+
+        return "<p>" . $this->paragrafo . "</p>";
+    }
+}
+
+class TagViewhelper extends AbstractViewHelper
+{
+
+    public function render()
+    {
+        return "<$this->tag>" . $this->contenuto . "<$this->tag/>";
+    }
+}
+
+
+$pargrafo = new PViewHelper();
+
+$pargrafo->render();
+
