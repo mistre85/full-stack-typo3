3210a45479af1f0f735611fc9ab2f4a26ed2e6b8
Orlando Giuseppe <giuseppe.orlando@h3g.it>
2021-03-18 12:27:14 +0100

    CompanySocialNetwork.php

--- company-social-network/htdocs/typo3conf/ext/company_social_network/Classes/Utility/CompanySocialNetwork.php
+++ company-social-network/htdocs/typo3conf/ext/company_social_network/Classes/Utility/CompanySocialNetwork.php
@@ -2,13 +2,88 @@
 
 
 namespace Wind\Csnd\Utility;
+
+
 use Wind\Csnd\Domain\Model\User;
 
-class CompanySocialNetwork{
 
-    public static function registerUserCookie(User $userFound){
-    
-        setcookie('user', $userFound->getUid(), 0, '/', "typo.local");
+/**
+ * Class CompanySocialNetwork
+ *
+ * Collettore di scenari di flusso dati
+ * contenenitore di funzione
+ *
+ * @package Wind\Csnd\Utility
+ */
+class CompanySocialNetwork
+{
+    /**
+     * userRepository
+     *
+     * @var \Wind\Csnd\Domain\Repository\UserRepository
+     * @inject
+     */
+    protected $userRepository = null;
+
+
+    /**
+     * Controlla il cookie e verifica se l'utente esiste
+     * @return boolean
+     */
+    public function isUserLogged()
+    {
+        $user = $this->getLoggedUser();
+
+        ///logica di access token
+
+        if (empty($user)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function getLoggedUser()
+    {
+        $userCookie = self::readUserCookie();
+
+        if (empty($userCookie)) {
+            return false;
+        }
+
+        /** @var User $user */
+        $user = $this->userRepository->findByUid($userCookie);
+
+        return $user;
+    }
+
+    public static function readUserCookie()
+    {
+        $userCookie = CompanySocialNetwork::readCookie('user');
+        return $userCookie;
+    }
+
+
+    /**
+     * @param User $userFound
+     */
+    public static function registerUserCookie(User $userFound)
+    {
+        setcookie('user', $userFound->getUid(), 0, '/', "localhost");
+    }
+
+    public static function readCookie($name)
+    {
+        if (empty($name)) {
+            return false;
+        }
+
+        return $_COOKIE[$name];
+    }
+
+    public static function deleteCookie($cookieName)
+    {
+        setcookie($cookieName, "", -1, '/', "localhost");
     }
 
 
